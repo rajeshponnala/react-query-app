497bc877ea9a70773266e2787bfa7d1146b4854d
rajesh <rajeshreddyponnala@gmail.com>
2021-10-03 15:32:59 +0530

    Init Project;Add react query with pokemon api and component;Disable automatically refetching queries on window focus;Add react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import axios, { AxiosResponse } from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Pokemon() {
+  const queryInfo = useQuery("pokemon", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    return axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((res: AxiosResponse<any>) => res.data.results);
+  }, {
+    refetchOnWindowFocus: false
+  });
+  return (
+    <div>
+      {queryInfo.isLoading ? (
+        <div>loading...</div>
+      ) : (
+        queryInfo.data.map((result: { name: string }) => (
+          <div key={result.name}>{result.name}</div>
+        ))
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <Pokemon/>
+        <ReactQueryDevtools/>
+      </body>
     </div>
   );
 }
